96f332344f55715e38a012797c28461a46a25973
abhiraj <abhiaditya860@gmail.com>
2026-04-19 00:15:55 +0530

    abstraction

---
+++ oops/Abstraction.go
@@ -0,0 +1,37 @@
+// Abstraction: Define interface for variations. Multiple payments methods? Different vehicle types? Create an interface
+
+package main
+
+type PaymentMethod interface {
+	Process(amount float64) bool
+}
+
+type CreditCardPayment struct{}
+
+func (CreditCardPayment) Process(amount float64) bool {
+	_ = amount
+	return true
+}
+
+type PayPalPayment struct{}
+
+func (PayPalPayment) Process(amount float64) bool {
+	_ = amount
+	return true
+}
+
+type GoodOrder struct {
+	Total float64
+}
+
+type OrderServiceGood struct {
+	paymentMethod PaymentMethod
+}
+
+func NewOrderServiceGood(method PaymentMethod) *OrderServiceGood {
+	return &OrderServiceGood{paymentMethod : method}
+}
+
+func (s *OrderServiceGood) Checkout(order GoodOrder) {
+	s.paymentMethod.Process(order.Total)
+}
