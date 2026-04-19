94fde70fe35a8568435c636d60d0e8d350e4b22e
abhiraj <abhiaditya860@gmail.com>
2026-04-19 12:06:46 +0530

    ISP

---
+++ design-principles/ISP.go
@@ -0,0 +1,27 @@
+// SOLID Principles
+// - ISP - Keep interfaces clean and focussed
+
+package main
+
+type Workable interface {
+	Work()
+}
+
+type Feedable interface {
+	Eat()
+}
+
+type Restable interface {
+	Sleep()
+}
+ 
+
+type Human struct{}
+
+func (Human) Work() {}
+func (Human) Eat() {}
+func (Human) Sleep() {}
+
+type Robot struct{}
+
+func (Robot) Work() {}
