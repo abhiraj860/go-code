06a8883fce065304fb9c8c439a557b6711e2aee5
abhiraj <abhiaditya860@gmail.com>
2026-04-19 11:29:01 +0530

    separation of concern

---
+++ design-principles/SeparationOfConcerns.go
@@ -0,0 +1,49 @@
+// Separation of Concerns - Enable independent testing and changes
+
+package main
+
+type Board interface {
+	HasWinner() bool
+	MakeMove(move string)
+	GetWinner() string
+}
+
+type Display interface {
+	Render(board Board)
+	ShowWinner(winner string)
+}
+
+type InputHandler interface {
+	NextMove() string
+}
+
+type TicTacToeGood struct {
+	board Board
+	display Display
+	inputHandler InputHandler
+}
+
+func NewTicTacToeGood(board Board, display Display, inputHandler InputHandler) *TicTacToeGood {
+	return &TicTacToeGood{
+		board: board,
+		display: display,
+		inputHandler: inputHandler,
+	}
+}
+
+func (g *TicTacToeGood) Play() {
+	for !g.board.HasWinner() {
+		g.display.Render(g.board)
+		move := g.inputHandler.NextMove()
+		g.board.MakeMove(move)
+	}
+	g.display.ShowWinner(g.board.GetWinner())
+}
+
+
+
+
+
+
+
+
