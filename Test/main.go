ba34a02d04d2cc94e9d9c8ea02da89df8f43b8a2
abhiraj <abhiaditya860@gmail.com>
2026-05-16 22:03:50 +0530

    go fund

---
+++ Test/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func someFunc(num string) {
+	fmt.Println(num)
+}
+
+
+func main() {
+	go someFunc("1")
+	go someFunc("2")
+	go someFunc("3")
+
+	fmt.Println("hi")
+}
