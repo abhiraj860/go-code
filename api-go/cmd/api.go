f763dda027274ff57e771f410648cde9132903ab
abhiraj <abhiaditya860@gmail.com>
2026-05-25 16:10:16 +0530

    api

---
+++ api-go/cmd/api.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"github.com/go-chi/chi/v5"
+)
+
+type application struct {
+	config config
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
