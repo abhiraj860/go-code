2c3af5806ddc371d7816b2ae2cbf9813de97a8c7
abhiraj <abhiaditya860@gmail.com>
2026-05-30 15:48:04 +0530

    bs

--- Test/main.go
+++ Test/main.go
@@ -1,9 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Golanf");
+	nums := []int{2, 3, 4, 5, 5}
+	s := 0
+	e := len(nums) - 1
+	k := 0
+	fmt.Scanln(&k)
+	found := false
+	mid := -1
+	for s <= e {
+		mid = (s + e) / 2
+		if nums[mid] == k {
+			found = true
+			break
+		} else if nums[mid] < k {
+			s = mid + 1
+		} else {
+			e = mid - 1
+		}
+	}
+	if found {
+		fmt.Println(mid)
+	} else {
+		fmt.Println(-1)
+	}
 }
