8161be6b7e771c8b32b659cf361f994b61b411f2
abhiraj <abhiaditya860@gmail.com>
2026-05-31 15:37:31 +0530

    vowels

--- Test/main.go
+++ Test/main.go
@@ -2,13 +2,31 @@ package main
 
 import "fmt"
 
+
+func vowelStrings(word string, queries [][]int) []int {
+	n := len(word)
+	vowelCnt := make([]int, n + 1)
+	for i, ch := range word {
+		switch ch {
+			case 'a', 'e', 'i', 'o', 'u':
+				vowelCnt[i + 1] = vowelCnt[i] + 1
+			default:
+				vowelCnt[i + 1] = vowelCnt[i] 
+		}
+	}
+	res := []int{}
+	for _, que := range queries {
+		i := que[0]
+		j := que[1]
+		res = append(res, vowelCnt[j + 1] - vowelCnt[i])
+	}
+	
+	return res
+}
+
+
 func main() {
-	arr := [3]int{1, 2, 3}
-	v := []int{1,2 , 3}
-	v = append(v, 4)
-	a := make([]int, 4, 5)
-	fmt.Println(a)
-	v[0] = 10
-	fmt.Println(len(v))
-	fmt.Println(arr)
+	word := "prefixsum"
+	queries := [][]int{{0, 2}, {1, 4}, {3, 5}}
+	fmt.Println(vowelStrings(word, queries))
 }
