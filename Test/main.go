0b995b828854d1b65e1a67f0eb88a4d3ba1419e0
abhiraj <abhiaditya860@gmail.com>
2026-06-13 22:01:27 +0530

    context-process-req

--- Test/main.go
+++ Test/main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 )
 
 func main() {
-	ctx := context.WithValue(context.Background(), "userId", "Abhiraj")
-	go getValue(ctx)
-	time.Sleep(10 * time.Second)
+	ctx := context.WithValue(context.Background(), "userId", 12334234)
+	processRequest(ctx)
 }
 
-func getValue(ctx context.Context) {
-	userId := ctx.Value("userId")
-	fmt.Println("UserID", userId)
+func processRequest(ctx context.Context) {
+	userId := ctx.Value("userId").(int)
+	fmt.Println("The name of the user is", userId)
 }
