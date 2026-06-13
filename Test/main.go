e2bdd08ec62d5370a6eef9460aea84afbac86dd9
abhiraj <abhiaditya860@gmail.com>
2026-06-13 20:34:34 +0530

    context_timeout

--- Test/main.go
+++ Test/main.go
@@ -1,42 +1,27 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"net/http"
+	"context"
 	"time"
 )
 
-func fetchAPI(ctx context.Context, url string, results chan<-string) {
-	req, err := http.NewRequestWithContext(ctx, "GET", url, nil) 
-	if err != nil {
-		results <- fmt.Sprintf("Error creating request for %s: %s", url, err.Error())
-		return
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2 *  time.Second)
+	defer cancel()
+	success := false
+	go performTask(ctx, &success)
+
+	
+	<-ctx.Done()
+	if success == false {
+		fmt.Println("Time out limit")
 	}
-	client := http.DefaultClient
-	resp, err := client.Do(req)
-	if err != nil {
-		results <- fmt.Sprintf("Error making request to %s: %s", url, err.Error())
-		return
-	}
-	defer resp.Body.Close()
-	results <- fmt.Sprintf("Result from %s, %d",url, resp.StatusCode)
+
 }
 
-func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-	defer cancel()
-	urls := []string{
-		"http://api.example.com/users",
-		"http://api.example.com/products",
-		"https://api.example.com/orders",
-	}
-	results := make(chan string)
-
-	for _, url := range urls {
-		go fetchAPI(ctx, url, results)
-	}
-	for range urls {
-		fmt.Println(<-results)
-	}
+func performTask(ctx context.Context, success *bool) {
+	<-time.After(5 * time.Second)
+	fmt.Println("Task completed successfully")
+	*success = true
 }
