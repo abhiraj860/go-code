207f51cdc850131eb2b63b10adf31a731f08e3d5
abhiraj <abhiaditya860@gmail.com>
2026-06-13 22:15:13 +0530

    cancel-context

--- Test/main.go
+++ Test/main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func main() {
-	ctx := context.WithValue(context.Background(), "userId", 12334234)
-	processRequest(ctx)
+	ctx, cancel := context.WithCancel(context.Background())
+	
+	go doTask(ctx)
+	time.Sleep(2 * time.Second)
+	cancel()
+	time.Sleep(2 * time.Second)
 }
 
-func processRequest(ctx context.Context) {
-	userId := ctx.Value("userId").(int)
-	fmt.Println("The name of the user is", userId)
+func doTask(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Task Cancelled")
+			return
+		default:
+			time.Sleep(500 * time.Millisecond)
+			fmt.Println("Task Processing...")
+		}
+
+	}
 }
