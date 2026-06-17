7ffc0606fda7090121a73ee5c500bdade46fa09d
abhiraj <abhiaditya860@gmail.com>
2026-06-17 23:33:52 +0530

    worker-pool

--- Test/main.go
+++ Test/main.go
@@ -1,26 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+	"math/rand"
+)
 
-func binarySearch(nums []int, target int) int {
-	s := 0
-	e := len(nums) - 1
-	for s <= e {
-		mid := (s + e) / 2
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] < target {
-			s = mid + 1
-		} else {
-			e = mid - 1
-		}
+type Job struct {
+	ID int
+	Payload string
+}
+
+type Result struct {
+	JobID int
+	Output string
+	Err error
+}
+
+func workerPool() {
+	const numberWorkers = 3
+	const numJobs = 10
+
+	jobs := make(chan Job, numJobs)
+	results := make(chan Result, numJobs)
+	
+	wg := sync.WaitGroup{}
+	for w := range numberWorkers {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			for job := range jobs {
+				time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
+				results<- Result{
+					JobID: job.ID,
+					Output: fmt.Sprintf("worker-%d processed job %d", workerID, job.ID),
+				}
+			}
+		}(w)
 	}
-	return -1
+
+
+
+	for i := range numJobs {
+		jobs <- Job{ID: i, Payload: fmt.Sprintf("data-%d", i)}
+	}
+	close(jobs)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for r := range results {
+		fmt.Printf("[worker-pool] %v\n", r.Output)
+	}
 }
 
 func main() {
-	nums := []int{-1, 0, 3, 5, 9, 12}
-	fmt.Println(binarySearch(nums, 9))
-	fmt.Println(binarySearch(nums, -1))
-	fmt.Println(binarySearch(nums, 2))
+	workerPool()
 }
