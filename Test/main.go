d6f122536c07a5633f526ef11c8c6c281bffb1fd
abhiraj <abhiaditya860@gmail.com>
2026-06-19 20:18:30 +0530

    locking

--- Test/main.go
+++ Test/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
 	i := 0
 	j := 0
 	k := 0
+	var mu sync.Mutex
 	go func() {
 		for j < 100000 {
+			mu.Lock()		
 			i += 1
+			mu.Unlock()
 			j++
 		}
 	}()
 	for k < 100000 {
+		mu.Lock()
 		i += 1
+		mu.Unlock()
 		k++
 	}
 	time.Sleep(1000 * time.Millisecond)
