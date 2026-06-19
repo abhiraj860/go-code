2e003581165c87e0c88d5f79dd07f7dc48035470
abhiraj <abhiaditya860@gmail.com>
2026-06-19 20:43:25 +0530

    gorouting code 400ms

--- Test/main.go
+++ Test/main.go
@@ -13,24 +13,19 @@ func main() {
 	var wg sync.WaitGroup
 	start := time.Now()
 	for range 100000 {
-		// wg.Add(1)
-		// go func() {
-		// 	j := 0
-		// 	defer wg.Done()
-		// 	local := 0
-		// 	for j < 100000 {
-		// 		local++	
-		// 		j++
-		// 	}
-		// 	mu.Lock()
-		// 	i += local
-		// 	mu.Unlock()
-		// }()
-		j:= 0
-		for j < 100000 {
-			i++
-			j++
-		}
+		wg.Add(1)
+		go func() {
+			j := 0
+			defer wg.Done()
+			local := 0
+			for j < 100000 {
+				local++	
+				j++
+			}
+			mu.Lock()
+			i += local
+			mu.Unlock()
+		}()
 	} 
 	for k < 100000 {
 		mu.Lock()
