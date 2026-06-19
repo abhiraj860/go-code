72566ddf4442428a3bbba2920c5d19115be86ae0
abhiraj <abhiaditya860@gmail.com>
2026-06-19 17:07:29 +0530

    semaphore

--- Test/main.go
+++ Test/main.go
@@ -2,38 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
-	"context"
-	"math/rand"
 )
 
-func dbCall(ctx context.Context) (string, error) {
-	select {
-	case <-time.After(time.Duration(rand.Intn(50)) * time.Millisecond):
-		return "Done", nil
-	case <-ctx.Done():
-		return "", ctx.Err()
+func semaphores(workers int, tasks int) {
+	sem := make(chan struct{}, workers)
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	result := []int{}
+	for t := range tasks {
+			wg.Add(1)
+			go func(id int) {
+				defer wg.Done()
+				sem<-struct{}{}
+				defer func() {<-sem}()
+				mu.Lock()
+				time.Sleep(30 * time.Millisecond)				
+				result = append(result, t)
+				mu.Unlock()
+			}(t)	
 	}
+	wg.Wait()
+	fmt.Println(result)
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Millisecond)
-	defer cancel()
-	res, err := dbCall(ctx)
-	if err != nil {
-		fmt.Println("The activity was timed out")
-	} else {
-		fmt.Printf("Activity succeeded %v\n", res)
-	}
+	totalWorkers := 4
+	totalTasks := 12
 
-
-	ctx2, cancel2 := context.WithTimeout(context.Background(), 25 * time.Millisecond)
-	defer cancel2()
-
-	res2, err := dbCall(ctx2)
-	if err != nil {
-		fmt.Println("The activity was timed out")
-	} else {
-		fmt.Printf("Activity succeeded %v \n", res2)
-	}
+	semaphores(totalWorkers, totalTasks)
+	fmt.Printf("Semaphores with %v workers and %v tasks\n", totalWorkers, totalTasks)
 }
