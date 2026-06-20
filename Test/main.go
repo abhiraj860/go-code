9295435d733756746c60f0e6ac74689482f945cf
abhiraj <abhiaditya860@gmail.com>
2026-06-20 19:39:38 +0530

    heap

--- Test/main.go
+++ Test/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
+	"container/heap"
 )
 
-type mapKey struct {
-	i int
-	j int
+type pair struct {
+	num, count int
+}
+
+type MaxHeap []pair
+
+func (h MaxHeap) Len() int { return len(h)}
+func (h MaxHeap) Less(i, j int) bool {return h[i].count > h[j].count}
+func (h MaxHeap) Swap(i, j int) {h[i], h[j] = h[j], h[i]} 
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(pair))
+}
+func (h *MaxHeap) Pop() any {
+	x := (*h)[len(*h) - 1]
+	*h = (*h)[:len(*h) - 1]
+	return x
+}
+
+func (h *MaxHeap) Top() any {
+	return (*h)[0]
+}
+
+func topKFrequent(nums []int, k int) []int {
+	freqMap := make(map[int]int)
+	for _, num := range nums {
+		freqMap[num]++
+	}
+	maxHeap := &MaxHeap{}
+	for num, freq := range freqMap{
+		*maxHeap = append(*maxHeap, pair{num, freq})
+	}
+	heap.Init(maxHeap)
+	topK := []int{}
+	for range k {
+		topK = append(topK, heap.Pop(maxHeap).(pair).num)
+	}
+	return topK
 }
 
 func main() {
-	hashMap := make(map[mapKey]int)
-	key := mapKey{0, 1}
-	hashMap[key] = 5
-	val, found := hashMap[key]
-	fmt.Println(val, found)
-	fmt.Println(hashMap)
+	nums := []int{1, 1, 1, 2, 2, 3, 3, 3,3}
+	fmt.Println(topKFrequent(nums, 1))	
 }
