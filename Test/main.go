769ef36f94aacb85d3319663caa3a4d776bf097b
abhiraj <abhiaditya860@gmail.com>
2026-06-21 00:04:06 +0530

    hw

--- Test/main.go
+++ Test/main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"context"
 )
 
-func longRunningTask(ctx context.Context) {
-	select {
-	case <-time.After(20 * time.Millisecond):
-		fmt.Println("Task Completed")
-	case <-ctx.Done():
-		fmt.Println(ctx.Err())
-	}
-}
-
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Millisecond)
-	defer cancel()
-	longRunningTask(ctx)
-}
 
+	fmt.Println("Hello World")
+	fmt.Printf("Abhd %s\n", "workd")
+
+}
