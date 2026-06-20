e19ee32b0593ec60b0e50243dd5dd3fe9fc8b943
abhiraj <abhiaditya860@gmail.com>
2026-06-20 18:37:22 +0530

    slices

--- Test/main.go
+++ Test/main.go
@@ -1,40 +1,32 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
+	"slices"
 )
 
-type IntHeap []int
-
-func (h IntHeap) Len() int {return len(h)}
-func (h IntHeap) Less(i, j int) bool {return h[i] > h[j]}
-func (h IntHeap) Swap(i, j int) {h[i], h[j] = h[j], h[i]} 
-func (h *IntHeap) Push(x any) {*h = append(*h, x.(int))}
-func (h *IntHeap) Pop() any {
-	n := len(*h)
-	x := (*h)[n - 1]
-	(*h) = (*h)[0:n - 1]
-	return x
-}
-func (h *IntHeap) Peek() (int, bool) {
-	if len(*h) == 0 {
-		return 0, false
-	}
-	return (*h)[0], true
-}
-
 func main() {
-	hea := &IntHeap{}
-	arr := []int{1, 2, 3, 4, 5, 900, -121}
-	heap.Init(hea)
-	for _, v := range arr {
-		heap.Push(hea, v)
-	}
-	val, _ := hea.Peek()
-	fmt.Println(val)
-	fmt.Println(heap.Pop(hea))
-	for hea.Len() > 0 {
-		fmt.Println(heap.Pop(hea))
-	}
+	size := 10
+	slice := make([]int, size)
+	fmt.Printf("Slice (method1): %v \n", slice)
+	slice2 := make([]int, size)
+	fmt.Printf("Slice (method2): %v \n", slice2)
+	slice3 := []int{}
+	fmt.Printf("Slice (method3): %v \n", slice3)
+	slice4 := []int{4,7, 1, 8, 3, 0, 2, 67, 34, 4, 1}
+	slice4 = append(slice4, 11)
+	length := len(slice4)
+	fmt.Printf("Length %v \n", length)
+	fmt.Printf("Slice %v\n", slice4)
+	slice4 = slice4[:len(slice4) - 1]
+	slice4 = slice4[1:]
+	fmt.Println("Slice", slice4)
+	slices.Sort(slice4)
+	fmt.Println("Slice", slice4)
+	slices.SortFunc(slice4, func(left, right int)int {
+		return right - left
+	})
+	fmt.Println("Slices", slice4)
+	slices.Reverse(slice4)
+	fmt.Println("Slices", slice4)
 }
