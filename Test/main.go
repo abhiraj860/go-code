56242f9c380f42076652bd00b054aeafccc4ddf8
abhiraj <abhiaditya860@gmail.com>
2026-06-20 20:13:08 +0530

    context

--- Test/main.go
+++ Test/main.go
@@ -3,31 +3,21 @@ package main
 import (
 	"fmt"
 	"time"
+	"context"
 )
 
-
+func longRunningTask(ctx context.Context) {
+	select {
+	case <-time.After(20 * time.Millisecond):
+		fmt.Println("Task Completed")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	}
+}
 
 func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
-
-	go func() {
-		time.Sleep(20 * time.Millisecond)
-		ch1 <- "hello"
-	}()
-
-	go func() {
-		time.Sleep(10 * time.Millisecond)
-		ch2 <- "worls"
-	}()
-
-	select {
-	case msg1 := <-ch1:
-		fmt.Println("Msg1 ", msg1)
-	case msg1 := <- ch2:
-		fmt.Println("Msg2", msg1)
-	case <-time.After(100 * time.Millisecond):
-		fmt.Println("Time Out")
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Millisecond)
+	defer cancel()
+	longRunningTask(ctx)
 }
 
