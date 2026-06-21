fe5c35e51c1e5c8b88b627c0016e8bbb8f14961a
abhiraj <abhiaditya860@gmail.com>
2026-06-21 17:04:52 +0530

    buff-wg-channel

--- Test/main.go
+++ Test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 )
 
@@ -10,27 +11,33 @@ func process(v int) int {
 }
 
 func processChannel(ch chan int) []int {
-	const conc = 10
+	const conc = 3
 	result := make(chan int, conc)
+	var wg sync.WaitGroup
 
 	for range conc {
+		wg.Add(1)
 		go func() {
+			wg.Done()
 			v := <-ch
 			result <- process(v)
 		}()
 	}
+	wg.Wait()
+	close(result)
 	var out []int
-	for range conc {
-		out = append(out, <-result)
+	for v := range result {
+		out = append(out, v)
 	}
 	return out
 }
 
 func main() {
-	ch := make(chan int, 10)
-	for i := range 10 {
+	ch := make(chan int, 11)
+	for i := range 11 {
 		ch <- i + 1
 	}
+	close(ch)
 	result := processChannel(ch)
 
 	for _, v := range result {
