997022ddea2bfd42331153c5ba52d49b456549e7
abhiraj <abhiaditya860@gmail.com>
2026-06-21 21:44:30 +0530

    panic

--- Test/main.go
+++ Test/main.go
@@ -1,46 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-
-)
-
-func process(v int) int {
-	return 5 * v
-}
-
-func processChannel(ch chan int) []int {
-	const conc = 3
-	result := make(chan int, conc)
-	var wg sync.WaitGroup
-
-	for range conc {
-		wg.Add(1)
-		go func() {
-			wg.Done()
-			v := <-ch
-			result <- process(v)
-		}()
-	}
-	wg.Wait()
-	close(result)
-	var out []int
-	for v := range result {
-		out = append(out, v)
-	}
-	return out
-}
+import "fmt"
 
 func main() {
-	ch := make(chan int, 11)
-	for i := range 11 {
-		ch <- i + 1
-	}
-	close(ch)
-	result := processChannel(ch)
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered:", r)
+		}
+	}()	
 
-	for _, v := range result {
-		fmt.Println(v)
-	}
+	panic("boom")
 }
