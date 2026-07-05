b1167ce3cbf51550676cbfe7a647f8e62039c4c4
abhiraj <abhiaditya860@gmail.com>
2026-07-05 17:07:15 +0530

    lower_bound

--- Test/main.go
+++ Test/main.go
@@ -1,86 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"container/list"
-)
+import "fmt"
 
-type LFUEntry struct {
-	key, val, freq int
-}
-
-type LFUCache struct {
-	cap, minFreq int
-	keyMap map[int]*list.Element
-	freqMap map[int]*list.List
-}
-
-func NewLFUCache(cap int) *LFUCache {
-	return &LFUCache{
-		cap: cap,
-		keyMap: make(map[int]*list.Element),
-		freqMap: make(map[int]*list.List),
-	}
-}
-
-func (c *LFUCache) getList(freq int) *list.List {
-	if c.freqMap[freq] == nil {
-		c.freqMap[freq] = list.New()
-	}
-	return c.freqMap[freq]
-}
-
-func (c *LFUCache) increment(elem *list.Element) {
-	entry := elem.Value.(*LFUEntry)
-	freq := entry.freq
-	c.getList(freq).Remove(elem)
-	if c.freqMap[freq].Len() == 0 {
-		delete(c.freqMap, freq)
-		if c.minFreq == freq {
-			c.minFreq++
+func lowerBound(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	ans := len(nums)
+	for low <= high {
+		mid := low + (high - low) / 2
+		if nums[mid] >= target {
+			ans = mid
+			high = mid - 1
+		} else {
+			low = mid + 1
 		}
 	}
-	entry.freq++
-	newElem := c.getList(entry.freq).PushFront(entry)
-	c.keyMap[entry.key] = newElem
+	return ans
 }
 
-func (c *LFUCache) Get(key int) int {
-	elem, ok := c.keyMap[key]
-	if !ok {
-		return -1
-	}
-	c.increment(elem)
-	return c.keyMap[key].Value.(*LFUEntry).val
-}
 
-func (c *LFUCache) Put(key, val int) {
-	 if c.cap == 0 {
-		return 
-	 }
-	 if elem, ok := c.keyMap[key]; ok {
-		elem.Value.(*LFUEntry).val = val
-		c.increment(elem)
-		return
-	 }
-	 if len(c.keyMap) == c.cap {
-		lst := c.freqMap[c.minFreq]
-		evicted := lst.Back()
-		lst.Remove(evicted)
-		if lst.Len() == 0 {
-			delete(c.freqMap, c.minFreq)
-		}
-		delete(c.keyMap, evicted.Value.(*LFUEntry).key)
-	 }
-	 entry := &LFUEntry{key:key, val : val, freq:1}
-	 elem := c.getList(1).PushFront(entry)
-	 c.keyMap[key] = elem
-	 c.minFreq = 1
-
-}
 
 
 func main() {
-	fmt.Println("LFU")
-	
+	nums := []int{2, 3, 7, 10, 11, 11, 25}
+	target := 3
+	fmt.Println(lowerBound(nums, target))
 }
