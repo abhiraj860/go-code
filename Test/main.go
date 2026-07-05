0cf6116798ea1d22c00a50737b7366b0a217e888
abhiraj <abhiaditya860@gmail.com>
2026-07-05 18:55:03 +0530

    first_and_last_occurrence

--- Test/main.go
+++ Test/main.go
@@ -1,43 +1,47 @@
 package main
 
 import "fmt"
+func firstFn(nums []int, target int) int {
+    low := 0
+    high := len(nums) - 1
+    ans := -1
+    for low <= high {
+        mid := low + (high - low) / 2
+        if(nums[mid] == target) {
+            high = mid - 1
+            ans = mid
+        } else if nums[mid] < target {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return ans
+}
 
-func floor(nums []int, target int) int {
-	low := 0
-	high := len(nums) - 1
-	ans := -1
-	for low <= high {
-		mid := low + (high - low) / 2
-		if nums[mid] <= target {
-			ans = nums[mid]
-			low = mid + 1
-		} else {
-			high = mid - 1
-		}
-	}
-	return ans
+func secondFn(nums []int, target int) int {
+    low := 0
+    high := len(nums) - 1
+    ans := -1
+    for low <= high {
+        mid := low + (high - low) / 2
+        if (nums[mid] == target) {
+            ans = mid
+            low = mid + 1
+        } else if nums[mid] < target {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return ans
 }
-func ceil(nums []int, target int) int {
-	low := 0
-	high := len(nums) - 1
-	ans := -1
-	for low <= high {
-		mid := low + (high - low) / 2
-		if nums[mid] >= target {
-			ans = nums[mid]
-			high = mid - 1
-		} else {
-			low = mid + 1
-		}
-	}
-	return ans
-}
+
 
 func main() {
-	nums := []int{2, 4, 6, 8, 10, 12, 14}
-	target := 1
-	findFloor := floor(nums, target)
-	findCeil := ceil(nums, target)
-
-	fmt.Println(findFloor, findCeil)
+	nums := []int{}
+	target := 0
+	first := firstFn(nums, target)
+    last := secondFn(nums, target)
+    fmt.Println(first, last)
 }
