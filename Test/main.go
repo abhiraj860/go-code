c015f039bb0aeb582dc4513844ed8e33f52ff98c
abhiraj <abhiaditya860@gmail.com>
2026-07-05 17:36:04 +0530

    floor_and_deil

--- Test/main.go
+++ Test/main.go
@@ -2,14 +2,29 @@ package main
 
 import "fmt"
 
-func upperBound(nums []int, target int) int {
+func floor(nums []int, target int) int {
 	low := 0
 	high := len(nums) - 1
-	ans := len(nums)
+	ans := -1
 	for low <= high {
 		mid := low + (high - low) / 2
-		if nums[mid] > target {
-			ans = mid
+		if nums[mid] <= target {
+			ans = nums[mid]
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return ans
+}
+func ceil(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	ans := -1
+	for low <= high {
+		mid := low + (high - low) / 2
+		if nums[mid] >= target {
+			ans = nums[mid]
 			high = mid - 1
 		} else {
 			low = mid + 1
@@ -18,10 +33,11 @@ func upperBound(nums []int, target int) int {
 	return ans
 }
 
-
-
 func main() {
-	nums := []int{2, 3, 7, 10, 11, 11, 25}
-	target := 7
-	fmt.Println(upperBound(nums, target))
+	nums := []int{2, 4, 6, 8, 10, 12, 14}
+	target := 1
+	findFloor := floor(nums, target)
+	findCeil := ceil(nums, target)
+
+	fmt.Println(findFloor, findCeil)
 }
