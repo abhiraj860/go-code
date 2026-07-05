592f8a71e0821142332628b851d4f69eec8e9cdd
abhiraj <abhiaditya860@gmail.com>
2026-07-06 01:44:57 +0530

    find_rotated_times

--- Test/main.go
+++ Test/main.go
@@ -1,52 +1,32 @@
 package main
 
-import "fmt"
-
-func findLow(nums []int, target int) int {
-	low := 0
-	high := len(nums) - 1
-	ans := -1
-	for low <= high {
-		mid := low + (high - low) / 2
-		if nums[mid] == target {
-			ans = mid
-			high = mid - 1
-		} else if nums[mid] > target {
-			high = mid - 1
-		} else {
-			low = mid + 1
-		}
-	}
-	return ans
-}
-
-func findHigh(nums []int, target int) int {
-	low := 0
-	high := len(nums) - 1
-	ans := -1
-	for low <= high {
-		mid := low + (high - low) / 2
-		if nums[mid] == target {
-			ans = mid
-			low = mid + 1
-		} else if nums[mid] < target {
-			low = mid + 1
-		} else {
-			high = mid - 1
-		}
-	}
-	return ans
-}
+import (
+	"fmt"
+	"math"
+)
 
 
 func main() {
-	nums := []int{8, 9, 10, 12, 12, 12}
-	target := 12
-	low := findLow(nums, target)
-	high := findHigh(nums, target)
-	if low == -1 {
-		fmt.Println(0)
-	} else {
-		fmt.Println(high - low + 1)
+	nums := []int{3,4,5,6, 1,2}
+	low := 0
+	high := len(nums) - 1
+	ans := math.MaxInt32
+	indx := -1
+	for low <= high {
+		mid := low + (high - low) / 2
+		if nums[mid] <= nums[high] {
+			if ans >= nums[mid] {
+				ans = nums[mid]
+				indx = mid
+			}
+			high = mid - 1
+		} else {
+			if ans <= nums[low] {
+				ans = nums[low]
+				indx = low
+			}
+			low = mid + 1
+		}
 	}
+	fmt.Println(indx)
 }
