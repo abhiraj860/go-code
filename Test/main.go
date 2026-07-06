c40582761f2721808b911fb53c7b32fd190471c6
abhiraj <abhiaditya860@gmail.com>
2026-07-06 21:36:24 +0530

    abstraction

--- Test/main.go
+++ Test/main.go
@@ -1,46 +1,37 @@
+// Abstraction
+
 package main
 
-import (
-	"fmt"
-)
-
-func midN(mid int, n int, m int) int64 {
-	ans := int64(1)
-	for i := 1; i <= n; i++ {
-		ans = ans * int64(mid)
-		if ans > int64(m) {
-			return 2
-		} 
-	}
-	if ans == int64(m) {
-		return 1
-	}
-	if ans < int64(m) {
-		return 0
-	}
-	return ans
+type PaymentMethod interface {
+	Process(amount float64) bool
 }
 
-func findNthRoot(n int, m int) int {
-	low := 0
-	high := m
-	for low <= high {
-		mid := low + (high - low) / 2
-		val := midN(mid, n, m)
-		switch val {
-		case 1:
-			return mid
-		case 0:
-			low = mid + 1
-		default:
-			high = mid - 1
-		}
-	}
-	return -1
+type CreditCardPayment struct{}
+
+func (CreditCardPayment) Process(amount float64) bool {
+	_ = amount
+	return true
 }
 
-func main() {
-	n := 3
-	m := 343
-	fmt.Println(findNthRoot(n, m))
+type PayPalPayment struct{}
+
+func (PayPalPayment) Process(amount float64) bool {
+	_ = amount
+	return true
+}
+
+type GoodOrder struct {
+	total float64
+}
+
+type OrderServiceGood struct {
+	paymentMethod PaymentMethod
+}
+
+func NewOrderServiceGood(method PaymentMethod) *OrderServiceGood {
+	return &OrderServiceGood{paymentMethod: method}
+}
+
+func (s *OrderServiceGood) Checkout(order GoodOrder) {
+	s.paymentMethod.Process(order.total)
 }
