0a5f66b83b2ad2fd7eae4c94262857e32bbd2f91
abhiraj <abhiaditya860@gmail.com>
2026-07-12 00:41:37 +0530

    gas_station

--- dsa/main.go
+++ dsa/main.go
@@ -2,60 +2,70 @@ package main
 
 import (
 	"fmt"
+	"container/heap"
 )
 
-func possible(mid int, arr []int, k int) bool {
-	cnt := 1
-	curr := 0
-	for _, v := range arr {
-		if v + curr <= mid {
-			curr += v
-		} else {
-			cnt++
-			curr = v
-		}
-	}
-	if cnt > k {
-		return false
-	}
-	return true
+type pair struct {
+	dist float64
+	indx int
 }
 
-func part(arr []int, k int) int {
-	if k > len(arr) {
-		return -1
+type maxHeap []pair
+
+func (m maxHeap) Len() int {
+	return len(m)
+}
+
+func (m maxHeap) Less(i, j int) bool  {
+	return m[i].dist > m[j].dist
+}
+
+func (m maxHeap) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
+func (m *maxHeap) Push(x any) {
+	*m = append(*m, x.(pair))
+}
+
+func (m *maxHeap) Pop() any {
+	old := *m
+	n := len(old)
+	item := old[n - 1]
+	*m = old[:n - 1]
+	return item
+}
+
+func (m maxHeap) Peek() (pair, bool) {
+	if len(m) == 0 {
+		return pair{}, false
 	}
-	low := arr[0]
-	high := 0
-	for _, v := range arr {
-		low = max(low, v)
-		high += v
+	return m[0], true
+}
+
+func location(stations []int, k int) float64 {
+	pq := &maxHeap{}
+	heap.Init(pq)
+	for i := 1; i < len(stations); i++ {
+		dist := float64(stations[i]) - float64(stations[i - 1])
+		heap.Push(pq, pair{dist, i - 1})
+
 	}
-	ans := -1
-	for low <= high {
-		mid := low + (high - low) / 2
-		if possible(mid, arr, k) {
-			ans = mid
-			high = mid - 1
-		} else {
-			low = mid + 1
-		}
+	arr := make([]int, len(stations) - 1)
+	for k > 0 {
+		top := heap.Pop(pq).(pair)
+		getIndx := top.indx
+		arr[getIndx]++
+		newDist := (float64(stations[getIndx + 1]) - float64(stations[getIndx])) / (float64(arr[getIndx] + 1.0))
+		heap.Push(pq, pair{newDist, getIndx})
+		k--
 	}
-	return ans
+	i, _ := pq.Peek()
+	return i.dist
 }
 
 func main() {
-	arr := []int{5, 10, 30, 20, 15}
-	k := 3
-	arr2 := []int{10, 20, 30, 40}
-	k2 := 2
-	arr3 := []int{100, 200, 300, 400}
-	k3 := 1
-	arr4 := []int{5, 5, 5, 5}
-	k4 := 2
-	fmt.Println(part(arr, k))
-	fmt.Println(part(arr2, k2))
-	fmt.Println(part(arr3, k3))
-	fmt.Println(part(arr4, k4))
-
+	stations := []int{11,2,3,4,5,6,7,8,9,10}
+	k := 1
+	fmt.Println(location(stations, k))
 }
