ecdb635ebe037748d4faad1d1cc2bc0a5d695436
abhiraj <abhiaditya860@gmail.com>
2026-07-13 13:44:05 +0530

    find_shortest_subarray

--- dsa/main.go
+++ dsa/main.go
@@ -3,36 +3,59 @@ package main
 import (
 	"fmt"
 )
-type pair struct{
-	first string
-	second int
-}
 
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func badness(ranks []pair) int {
-	 bad := 0
-	 desRank := make([]int, len(ranks) + 1)
-	 for _, v := range ranks {
-		desRank[v.second]++
-	 }
-	 pos := 0
-	 for i := 1; i <= len(ranks); i++ {
-		for desRank[i] > 0 {
-			pos++
-			bad += abs(pos - i)
-			desRank[i]--
+func findShort(nums []int) int {
+	s := 0
+	n := len(nums)
+	for s < n  {
+		if s < n - 1 && nums[s] > nums[s + 1] {
+			break	
 		}
-	 }
-	 return bad
+		s++ 	
+	}
+	if s == n {
+		return 0
+	}
+	e := len(nums) - 1
+	for e > 0 {
+		if nums[e] < nums[e - 1] {
+			break
+		} 
+		e--
+	}
+	maxm := nums[s]
+	minm := nums[s]
+	for k := s + 1; k <= e; k++ {
+		maxm = max(maxm, nums[k])
+		minm = min(minm, nums[k])
+	}
+	i := 0
+	for i <= s {
+		if nums[i] > minm {
+			break
+		}
+		i++
+	}
+	j := n - 1
+	for j >= e {
+		if nums[j] < maxm {
+			break
+		}
+		j--
+	}
+	return j - i + 1
+
 }
 
 func main() {
-	ranks := []pair{{"A", 1}, {"B", 2}, {"W", 2}, {"B", 1}, {"D", 5}, {"S", 7}, {"W", 7}}
-	fmt.Println(badness(ranks))
+	nums := []int{2, 6, 4, 8, 10, 9, 15}
+	fmt.Println(findShort(nums))
+	nums2 := []int{1, 2, 3, 4}
+	fmt.Println(findShort(nums2))
+	nums3 := []int{1}
+	fmt.Println(findShort(nums3))
+	nums4 := []int{0, 2, 4, 7, 10, 11, 7, 12, 13, 14, 16, 19, 29}
+	fmt.Println(findShort(nums4))
+	nums5 := []int{2, 1}
+	fmt.Println(findShort(nums5))
 }
