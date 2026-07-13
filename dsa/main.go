bdb839f8953abaa62deb0ed7f9db879f806d4e30
abhiraj <abhiaditya860@gmail.com>
2026-07-13 11:53:01 +0530

    sparse_search

--- dsa/main.go
+++ dsa/main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"fmt"
-	"slices"
-	"cmp"
 )
 
-func smallestString(words []string) string {
-	slices.SortFunc(words, func(a,b string) int {
-		return cmp.Compare(a + b, b + a)
-	})
-	res := ""
-	for _, w := range words {
-		res = res + w
+func sparseSearch(words []string, s int, e int, key string) int {
+	for s <= e {
+		mid := s + (e - s) / 2
+		midLeft := mid - 1
+		midRight := mid + 1
+		if words[mid] == "" {
+			for true {
+				if midLeft < s && midRight > e {
+					return -1
+				} else if midLeft >= s && words[midLeft] != "" {
+					mid = midLeft
+					break
+				} else if midRight <= e && words[midRight] != "" {
+					mid = midRight
+					break
+				}
+				midLeft--
+				midRight++
+			}
+		}
+		if words[mid] == key {
+			return mid
+		} else if key > words[mid] {
+			s = mid + 1
+		} else {
+			e = mid - 1
+		}
 	}
-	return res
+	return -1
 }
 
 func main() {
-	words := []string{"c", "cb", "cba"}
-	fmt.Println(smallestString(words))
-	words1 := []string{"a", "ab", "aba"}
-	fmt.Println(smallestString(words1))
+	words := []string{"ai", "", "", "bat", "", "", "car", "cat", "", "", "dog", "", "e"}
+	k := "car"
+	fmt.Println(sparseSearch(words, 0, len(words) - 1, k))
 }
