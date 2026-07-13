bdf4651ab086a9dea1e06f4667047446eb187295
abhiraj <abhiaditya860@gmail.com>
2026-07-13 11:42:40 +0530

    smalest_string

--- dsa/main.go
+++ dsa/main.go
@@ -2,34 +2,24 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"cmp"
 )
 
-func partition(nums []int, k int, s int, e int) int {
-	i := s - 1
-	pivot := nums[e]
-	for j := s; j < e; j++{
-		if nums[j] < pivot {
-			i++
-			nums[j], nums[i] = nums[i], nums[j]
-		}
+func smallestString(words []string) string {
+	slices.SortFunc(words, func(a,b string) int {
+		return cmp.Compare(a + b, b + a)
+	})
+	res := ""
+	for _, w := range words {
+		res = res + w
 	}
-	nums[i + 1], nums[e] = nums[e], nums[i + 1]
-	return i + 1
-}
-
-func quickSelect(nums []int, k int, s int, e int) int {
-	p := partition(nums, k, s, e)
-	if p == k {
-		return nums[p]
-	} else if k < p {
-		return quickSelect(nums, k, s, p - 1)
-	} else {
-		return quickSelect(nums, k, p + 1, e)
-	}
+	return res
 }
 
 func main() {
-	nums := []int{7, 10, 4, 3, 20, 15}
-	k := 4
-	fmt.Println(quickSelect(nums, k - 1, 0, len(nums) - 1))
+	words := []string{"c", "cb", "cba"}
+	fmt.Println(smallestString(words))
+	words1 := []string{"a", "ab", "aba"}
+	fmt.Println(smallestString(words1))
 }
