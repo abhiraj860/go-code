be5def497f29eef8f005b6a88f02cb923f0055fa
abhiraj <abhiaditya860@gmail.com>
2026-07-15 00:37:55 +0530

    housing

--- dsa/main.go
+++ dsa/main.go
@@ -4,58 +4,30 @@ import (
 	"fmt"
 )
 
-func findShort(nums []int) int {
-	s := 0
-	n := len(nums)
-	for s < n  {
-		if s < n - 1 && nums[s] > nums[s + 1] {
-			break	
-		}
-		s++ 	
-	}
-	if s == n {
-		return 0
-	}
-	e := len(nums) - 1
-	for e > 0 {
-		if nums[e] < nums[e - 1] {
-			break
-		} 
-		e--
-	}
-	maxm := nums[s]
-	minm := nums[s]
-	for k := s + 1; k <= e; k++ {
-		maxm = max(maxm, nums[k])
-		minm = min(minm, nums[k])
-	}
+func output(nums []int, k int)[][]int {
+	result := [][]int{}
 	i := 0
-	for i <= s {
-		if nums[i] > minm {
-			break
+	j := 0
+	curr := 0
+	for j < len(nums) {
+		curr += nums[j]
+		for curr > k && i < j {
+			curr -= nums[i]
+			i++
 		}
-		i++
-	}
-	j := n - 1
-	for j >= e {
-		if nums[j] < maxm {
-			break
+		if curr == k {
+			result = append(result, []int{i, j})
 		}
-		j--
+		j++
 	}
-	return j - i + 1
-
+	return result
 }
 
 func main() {
-	nums := []int{2, 6, 4, 8, 10, 9, 15}
-	fmt.Println(findShort(nums))
-	nums2 := []int{1, 2, 3, 4}
-	fmt.Println(findShort(nums2))
-	nums3 := []int{1}
-	fmt.Println(findShort(nums3))
-	nums4 := []int{0, 2, 4, 7, 10, 11, 7, 12, 13, 14, 16, 19, 29}
-	fmt.Println(findShort(nums4))
-	nums5 := []int{2, 1}
-	fmt.Println(findShort(nums5))
+	plots := []int{1, 3, 2, 1, 4, 1, 3, 2, 1, 1, 2}
+	k := 8
+	out := output(plots, k)
+	for _, v := range out {
+		fmt.Println(v)
+	}
 }
