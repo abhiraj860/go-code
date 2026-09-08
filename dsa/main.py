53ff20b9b7615e9bb14dbc6ea827cc61e5c223bc
abhiraj <abhiaditya860@gmail.com>
2026-09-08 19:20:16 +0530

    variables

--- dsa/main.py
+++ dsa/main.py
@@ -1,12 +1,18 @@
-def mergedIntervals(intervals):
-    sortedIntervals = sorted(intervals, key=lambda x: x[0])
-    merged = []
-    for interval in sortedIntervals:
-        if not merged or interval[0] > merged[-1][1]:
-            merged.append(interval)
-        else:
-            merged[-1][1] = max(interval[1], merged[-1][1])
-    return merged
+n = 0
+print('n =', n)
 
+n = 'abc'
+print('n =', n)
 
-print(mergedIntervals([[3,5],[1,4],[7,9],[6,8]]))
+n, m = 0, 'abc'
+print(n, m)
+
+n, m, z = 0.125, "abc", False
+print(n, m, z)
+
+n = n + 1
+print(n)
+n += 1
+print(n)
+n += 1
+print(n)
