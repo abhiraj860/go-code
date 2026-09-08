8bc48d13beb05c410c2418fdad5fc68db998a514
abhiraj <abhiaditya860@gmail.com>
2026-09-08 23:46:19 +0530

    map

--- dsa/main.py
+++ dsa/main.py
@@ -1,16 +1,25 @@
-mySet = set()
-mySet.add(5)
-mySet.add(1)
-mySet.add(2)
-print(mySet)
-print(len(mySet))
-print(1 in mySet)
-print(2 in mySet)
-print(3 in mySet)
-mySet.add(3)
-print(3 in mySet)
-mySet.remove(2)
-print(2 in mySet)
-print(set([1, 2, 4]))
-mySet = {i for i in range(5)}
-print(mySet)
+myMap = {}
+myMap["alice"] = 88
+myMap["bob"] = 77
+print(myMap)
+print(len(myMap))
+myMap["alice"] = 80
+print(myMap)
+print("alice" in myMap)
+myMap.pop("alice")
+print("alice" in myMap)
+
+myMap = {"alice": 90, "bob": 70}
+print(myMap)
+
+myMap = {i : 2 * i for i in range(3)}
+print(myMap)
+
+myMap = {"alice": 90, "bob": 70}
+for key in myMap:
+    print(key, myMap[key])
+for value in myMap.values():
+    print(value)
+    
+for key, val in myMap.items():
+    print(key, val)
