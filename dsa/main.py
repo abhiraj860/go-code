6575c713d1bb63fdbe0f7718535bddbaa8390926
abhiraj <abhiaditya860@gmail.com>
2026-09-08 19:16:22 +0530

    merged

--- dsa/main.py
+++ dsa/main.py
@@ -1,1 +1,12 @@
-print("Abhiraj")
+def mergedIntervals(intervals):
+    sortedIntervals = sorted(intervals, key=lambda x: x[0])
+    merged = []
+    for interval in sortedIntervals:
+        if not merged or interval[0] > merged[-1][1]:
+            merged.append(interval)
+        else:
+            merged[-1][1] = max(interval[1], merged[-1][1])
+    return merged
+
+
+print(mergedIntervals([[3,5],[1,4],[7,9],[6,8]]))
