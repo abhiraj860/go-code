924f252f433dd6b98df89c3cf5639e3fc266f836
abhiraj <abhiaditya860@gmail.com>
2026-09-09 19:11:28 +0530

    tuple

--- dsa/main.py
+++ dsa/main.py
@@ -1,25 +1,8 @@
-myMap = {}
-myMap["alice"] = 88
-myMap["bob"] = 77
-print(myMap)
-print(len(myMap))
-myMap["alice"] = 80
-print(myMap)
-print("alice" in myMap)
-myMap.pop("alice")
-print("alice" in myMap)
+tup = (1, 2, 3)
+print(tup)
 
-myMap = {"alice": 90, "bob": 70}
-print(myMap)
+print(tup[0])
+print(tup[1])
+print(tup[-1])
 
-myMap = {i : 2 * i for i in range(3)}
-print(myMap)
-
-myMap = {"alice": 90, "bob": 70}
-for key in myMap:
-    print(key, myMap[key])
-for value in myMap.values():
-    print(value)
-    
-for key, val in myMap.items():
-    print(key, val)
+print(tup[len(tup) - 1])
