cd1df5ef3a8fd1c3b1421d08566b2c517d123fda
abhiraj <abhiaditya860@gmail.com>
2026-09-11 20:01:55 +0530

    heap

--- dsa/main.py
+++ dsa/main.py
@@ -1,14 +1,22 @@
-def isValid(s):
-    stack = []
-    mapping = {")" : "(", "}" : "{", "]" : "["}
-    for char in s:
-        if char in mapping:
-            if not stack or stack[-1] != mapping[char]:
-                return False
-            stack.pop()
-        else:
-            stack.append(char)
-    return True
+import heapq
 
+minHeap = []
+heapq.heappush(minHeap, 3)
+heapq.heappush(minHeap, 2)
+heapq.heappush(minHeap, 4)
+# print(minHeap[0])
 
-print(isValid("(){({}})"))
+maxHeap = []
+heapq.heappush(maxHeap, -3)
+heapq.heappush(maxHeap, -2)
+heapq.heappush(maxHeap, -4)
+
+# while len(maxHeap):
+#     print(heapq.heappop(maxHeap) * -1)
+
+arr = [2, 1, 8, 4, 5]
+arr = [-x for x in arr]    
+
+heapq.heapify(arr)
+while arr:
+    print(-1 * heapq.heappop(arr))
