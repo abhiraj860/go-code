c164b9e5e724d197000a61a6bd79d729a157813f
abhiraj <abhiaditya860@gmail.com>
2026-09-11 20:10:43 +0530

    set

--- dsa/main.py
+++ dsa/main.py
@@ -1,14 +1,17 @@
-tup = (1, 2, 3)
-# print(tup)
-# print(tup[0])
-# print(tup[1])
-# print(tup[2])
-
-myMap = {(1, 2): 3, (1, 2): 6}
-print(myMap[(1, 2)])
-
 mySet = set()
 mySet.add(1)
-mySet.add(0)
+mySet.add(2)
+mySet.add(3)
+
 print(mySet)
-print((1, 2) in mySet)
+print(len(mySet))
+print(1 in mySet)
+print(2 in mySet)
+print(4 in mySet)
+
+mySet.remove(2)
+print(2 in mySet)
+
+print(set([1, 2, 3, 4, 4, 4]))
+mySet = {i for i in range(7)}
+print(mySet)
