5bf83e893a0a77ae3d03da75b01840dfc1a7baff
abhiraj <abhiaditya860@gmail.com>
2026-09-12 12:09:07 +0530

    koko_eating_banans

--- dsa/main.py
+++ dsa/main.py
@@ -1,14 +1,24 @@
-def bs(nums, target):
-    start = 0
-    end = len(nums) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return -1
+import math
+apples = [3, 6, 7]
+h = 8
 
-print(bs([1, 2, 3, 4], 1))
+def minRate(apples, h):
+    low = 1
+    high = max(apples)
+    ans = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if possible(mid, apples, h):
+            high = mid - 1
+            ans = mid
+        else:
+            low = mid + 1
+    return ans
+
+def possible(mid, apples, h):
+    total = 0
+    for app in apples:
+        total += math.ceil(app / mid)
+    return total <= h
+
+print(minRate( [30,11,23,4,20], 6))
