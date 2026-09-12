d9bbf183921a22ac2de952f472ee54e6a4d1cb06
abhiraj <abhiaditya860@gmail.com>
2026-09-12 14:07:05 +0530

    lst_done

--- dsa/main.py
+++ dsa/main.py
@@ -1,24 +1,16 @@
-marks = [3, 5, 6, "Abhira", True, 34, 34, 34, 12, 23,14, 56]
-# print(marks)
-# print(type(marks))
-# print(marks[0])
-# print(marks[1])
-# print(marks[2])
-# print(marks[3])
-# print(marks[4])
-# print(marks[5-3])
-# print(marks[-3])
-# if "6" in marks:
-#     print("Yes")
-# else:
-#     print("No")
-
-# if "Ha" in "Harry":
-#     print("Yes")
-
-# print(marks[:7])
-# print(marks[1:len(marks) + 10:3])
-
-lst = [i * i for i in range(11) if i % 2 == 0]
-lst = [i * i for i in range(10) for k in range(3)]
+lst = [11, 11, 11, 23, 4, 6]
 print(lst)
+# lst.append(7)
+# lst.sort(reverse=True)
+# lst.reverse()
+# m = lst.copy()
+# m[0] = 0
+# print(m)
+# print(lst)
+# lst.insert(1, 899)
+# print(lst)
+m = [900,5000, 1000, 1100]
+# lst.extend(m)
+# print(lst, m)
+k = lst + m
+print(k)
