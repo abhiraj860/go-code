7574b61a580b0c1981643e8d6fd495cc4ac9e68d
abhiraj <abhiaditya860@gmail.com>
2026-09-12 08:15:55 +0530

    arrays

--- dsa/main.py
+++ dsa/main.py
@@ -1,23 +1,79 @@
-def myFunc(m, n):
-    return m * n
-print(myFunc(3, 4))
+# arr = [1, 2, 3]
+# print(arr)
 
-def outer(a, b):
-    c = "c"
-    def inner():
-        return a + b + c
-    return inner()
-print(outer("a", "b"))
+# arr.append(4)
+# arr.append(5)
+# print(arr)
 
-def double(arr, val):
-    def helper():
-        for i, n in enumerate(arr):
-            arr[i] *= 2
-        # nonlocal val
-        nonlocal val
-        val *= 2
-    helper()
-    print(arr, val)
-nums = [1, 2]
-val = 3
-print(double(nums, val))
+# arr.pop()
+# print(arr)
+
+# arr.insert(1, 7)
+# print(arr)
+
+# arr[0] = 3
+# arr[3] = 0
+# print(arr)
+
+# n = 5
+# arr = [1] * n
+# print(arr)
+# print(len(arr))
+# arr = [1, 2, 3]
+# print(arr[-1])
+# print(arr[-2])
+
+
+# arr = [1, 2, 3, 4]
+# print(arr[1:3])
+# print(arr[0:4])
+# print(arr[0:10])
+
+# a, b, c = [1, 2, 3]
+# print(a, b, c)
+
+nums = [1, 2, 3]
+# for i in range(len(nums)):
+#     print(nums[i])
+# for n in nums:
+#     print(n)
+
+# for i, n in enumerate(nums):
+#     print(i, n)    
+
+# nums1 = [1, 2, 3, 5, 6]
+# nums2 = [5, 6, 76, 45, 45]
+# for n1, n2 in zip(nums1, nums2):
+#     print(n1, n2)
+    
+nums = [1, 2, 3]
+nums.reverse()
+print(nums)
+
+nums = [5, 4, 7, 3, 8]
+nums.sort()
+print(nums)
+
+
+nums.sort(reverse = True)
+print(nums)
+
+arr = ["zig","bob", "alice", "jane", "doe"]
+arr.sort()
+print(arr)
+
+arr.sort(key=lambda x: len(x))
+print(arr)
+
+
+arr = [i for i in range(5)]
+print(arr)
+
+# arr = [[0] * 4 for i in range(4)]
+# for k in range(len(arr)):
+#     print(arr[k])
+# print(arr[0][0], arr[3][3])
+
+arr = [[0] * 6] * 6
+for k in range(len(arr)):
+    print(arr[k])
