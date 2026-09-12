c7d396f4cd6149c69d823b7ee3699c4f33ba8128
abhiraj <abhiaditya860@gmail.com>
2026-09-12 08:27:18 +0530

    strings

--- dsa/main.py
+++ dsa/main.py
@@ -1,79 +1,14 @@
-# arr = [1, 2, 3]
-# print(arr)
+s = "abc"
+print(s[0:2])
 
-# arr.append(4)
-# arr.append(5)
-# print(arr)
+s += "def"
+print(s)
 
-# arr.pop()
-# print(arr)
+print(int("123") + int("565"))
+print(str(123) + str(123))
+print(ord("a"))
+print(ord("A"))
 
-# arr.insert(1, 7)
-# print(arr)
-
-# arr[0] = 3
-# arr[3] = 0
-# print(arr)
-
-# n = 5
-# arr = [1] * n
-# print(arr)
-# print(len(arr))
-# arr = [1, 2, 3]
-# print(arr[-1])
-# print(arr[-2])
-
-
-# arr = [1, 2, 3, 4]
-# print(arr[1:3])
-# print(arr[0:4])
-# print(arr[0:10])
-
-# a, b, c = [1, 2, 3]
-# print(a, b, c)
-
-nums = [1, 2, 3]
-# for i in range(len(nums)):
-#     print(nums[i])
-# for n in nums:
-#     print(n)
-
-# for i, n in enumerate(nums):
-#     print(i, n)    
-
-# nums1 = [1, 2, 3, 5, 6]
-# nums2 = [5, 6, 76, 45, 45]
-# for n1, n2 in zip(nums1, nums2):
-#     print(n1, n2)
-    
-nums = [1, 2, 3]
-nums.reverse()
-print(nums)
-
-nums = [5, 4, 7, 3, 8]
-nums.sort()
-print(nums)
-
-
-nums.sort(reverse = True)
-print(nums)
-
-arr = ["zig","bob", "alice", "jane", "doe"]
-arr.sort()
-print(arr)
-
-arr.sort(key=lambda x: len(x))
-print(arr)
-
-
-arr = [i for i in range(5)]
-print(arr)
-
-# arr = [[0] * 4 for i in range(4)]
-# for k in range(len(arr)):
-#     print(arr[k])
-# print(arr[0][0], arr[3][3])
-
-arr = [[0] * 6] * 6
-for k in range(len(arr)):
-    print(arr[k])
+strings = ["abu", "cd", "eddf"]
+strings.sort(key=lambda x : len(x), reverse = True)
+print(strings)
