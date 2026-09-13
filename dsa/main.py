316c544df1a2ffd6cbd480aa7b2bd8b41ef36b11
abhiraj <abhiaditya860@gmail.com>
2026-09-13 11:36:10 +0530

    for else

--- dsa/main.py
+++ dsa/main.py
@@ -1,28 +1,8 @@
-# dic = {
-#     344: "Harry",
-#     56: "Shubham",
-#     678: "zakir",
-#     567: "Neha",
-# }
-
-# print(dic[344])
-# print(dic.get(34))
-# print(dic.keys())
-# print(dic.values())
-
-# for key in dic.keys():
-#     print(f"The value corresponding to the key {key} is {dic[key]}")
-    
-    
-# print(dic.items())
-# for key, val in dic.items():
-#     print(key)
-
-ep = {122: 45, 344:56, 122:90, 5566: 89}
-ep2 = {122:56, 5566:900, "hhhe": "opop"}
-
-ep.update(ep2)
-print(ep)
-ep.popitem()
-del ep[122]
-print(ep)
+i = 0
+while i < 7:
+    print(i)
+    # if i == 4:
+    #     break
+    i += 1
+else:
+    print("Sorry no i", i)
