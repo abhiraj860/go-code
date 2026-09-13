a345beb40def3b46999b81652e0efed2341ca416
abhiraj <abhiaditya860@gmail.com>
2026-09-13 13:22:18 +0530

    try-except

--- dsa/main.py
+++ dsa/main.py
@@ -1,8 +1,11 @@
-i = 0
-while i < 7:
-    print(i)
-    # if i == 4:
-    #     break
-    i += 1
-else:
-    print("Sorry no i", i)
+a = input("Enter a number: ")
+
+try:
+    num = [5, 6]
+    print(num[int(a)])
+except ValueError:
+    print("Input is not an integer")
+except IndexError:
+    print("Index error")
+    
+
