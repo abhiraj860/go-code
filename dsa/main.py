8bccfd50158d3456124412f11baf65e0416576b7
abhiraj <abhiaditya860@gmail.com>
2026-09-13 13:42:09 +0530

    custom error

--- dsa/main.py
+++ dsa/main.py
@@ -1,13 +1,4 @@
-def fn():
-    try:
-        l = [1, 5, 6, 7]
-        i = int(input("Enter the index: "))
-        print(l[i])
-        return 1
-    except:
-        print("Some error occurred")
-        return -1
-    finally:
-        print("I am always executed")
-        
-print(fn())
+a = int(input("Enter any value between 5 and 9 "))
+
+if a < 5 or a > 9:
+    raise ValueError("Value should be between 5 and 9")
