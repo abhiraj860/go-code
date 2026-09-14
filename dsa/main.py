644727995734589123fa8cb57eebf1a950c61cb3
abhiraj <abhiaditya860@gmail.com>
2026-09-14 21:41:53 +0530

    k-closest

--- dsa/main.py
+++ dsa/main.py
@@ -1,2 +1,10 @@
-import pandas as pd
-print(pd.__version__)
+# import math
+# print(dir(math))
+def kClosest(nums, target, k):
+    dis = [(abs(target - v), v) for v in nums]
+    dis.sort()
+    return sorted([v for _, v in dis[0:k]])
+
+
+
+print(kClosest([5, 6, 7, 8, 9], 10, 2)) 
