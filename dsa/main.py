53026cdf7747fcaa4c3c5214d9c064fe43967768
abhiraj <abhiaditya860@gmail.com>
2026-09-14 22:09:51 +0530

    k-closest-pushpop

--- dsa/main.py
+++ dsa/main.py
@@ -1,14 +1,12 @@
 import heapq
 
 def kClosest(nums, target, k):
-    arr = [(-1 * abs(target - v), v) for v in nums[0:k]]  
+    arr = [(-abs(target - v), v) for v in nums[0:k]]  
     heapq.heapify(arr)  
     for v in nums[k:]:
         dist = abs(v - target)
-        topDist, _ = arr[0]
-        if dist < -1 * topDist:
-            heapq.heappush(arr, (-1 * dist, v)) 
-    arr = list(arr)
-    return sorted([-1 * v for _, v in arr])
+        if dist < -arr[0][0]:
+            heapq.heappushpop(arr, (-dist, v))
+    return sorted([v for _, v in arr])
 
 print(kClosest([-1, 0, 1, 4, 6], 1, 3))
