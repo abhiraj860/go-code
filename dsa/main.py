015aca405cdcbb60f4d0c0fb49433554b83e4aa1
abhiraj <abhiaditya860@gmail.com>
2026-09-14 21:56:09 +0530

    k-closest

--- dsa/main.py
+++ dsa/main.py
@@ -1,10 +1,14 @@
-# import math
-# print(dir(math))
+import heapq
+
 def kClosest(nums, target, k):
-    dis = [(abs(target - v), v) for v in nums]
-    dis.sort()
-    return sorted([v for _, v in dis[0:k]])
+    arr = [(-1 * abs(target - v), v) for v in nums[0:k]]  
+    heapq.heapify(arr)  
+    for v in nums[k:]:
+        dist = abs(v - target)
+        topDist, _ = arr[0]
+        if dist < -1 * topDist:
+            heapq.heappush(arr, (-1 * dist, v)) 
+    arr = list(arr)
+    return sorted([-1 * v for _, v in arr])
 
-
-
-print(kClosest([5, 6, 7, 8, 9], 10, 2)) 
+print(kClosest([-1, 0, 1, 4, 6], 1, 3))
