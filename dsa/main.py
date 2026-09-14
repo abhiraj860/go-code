9bf06cd50cd2a26cf40313e1c7474920bb9ec335
abhiraj <abhiaditya860@gmail.com>
2026-09-14 10:03:09 +0530

    virtual environment

--- dsa/main.py
+++ dsa/main.py
@@ -1,6 +1,2 @@
-marks = [12, 56, 32, 45, 1, 1, 4]
-for indx, val in enumerate(marks):
-    if indx == 3:
-        print("val", val)
-    else:
-        print(val)
+import pandas as pd
+print(pd.__version__)
