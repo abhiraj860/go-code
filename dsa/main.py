93e0e32ff4bb82cff25e3a7de6e9b726f6bc46d7
abhiraj <abhiaditya860@gmail.com>
2026-09-15 21:36:54 +0530

    lambda

--- dsa/main.py
+++ dsa/main.py
@@ -1,12 +1,16 @@
-import heapq
+# def double(x):
+#     return x * 2
 
-def kClosest(nums, target, k):
-    arr = [(-abs(target - v), v) for v in nums[0:k]]  
-    heapq.heapify(arr)  
-    for v in nums[k:]:
-        dist = abs(v - target)
-        if dist < -arr[0][0]:
-            heapq.heappushpop(arr, (-dist, v))
-    return sorted([v for _, v in arr])
+# double = lambda x : x * 2
+cube = lambda x : x**3
+# avg = lambda x, y, z : (x + y + z) / 3
 
-print(kClosest([-1, 0, 1, 4, 6], 1, 3))
+# print(avg(3, 5, 10))
+# print(double(5))
+# print(cube(5))
+
+def apple(fx, value):
+    return 6 + fx(value)
+
+print(apple(lambda x : x * x, 5))
+
