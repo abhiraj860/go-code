fef459df4364335f668e1461028520a047ce7ecd
abhiraj <abhiaditya860@gmail.com>
2026-09-15 22:26:33 +0530

    none and is

--- dsa/main.py
+++ dsa/main.py
@@ -1,6 +1,4 @@
-from functools import reduce
-numbers = [1, 2, 3, 4, 5]
-def mySum(x, y):
-    return x + y 
-sum = reduce(mySum , numbers)
-print(sum)
+a = None
+b = None
+print(a is None)
+print(a == b)
