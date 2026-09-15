cd9cb716778150f9707785e2713666a3bc099039
abhiraj <abhiaditya860@gmail.com>
2026-09-15 22:12:08 +0530

    map-filter

--- dsa/main.py
+++ dsa/main.py
@@ -1,16 +1,4 @@
-# def double(x):
-#     return x * 2
 
-# double = lambda x : x * 2
-cube = lambda x : x**3
-# avg = lambda x, y, z : (x + y + z) / 3
-
-# print(avg(3, 5, 10))
-# print(double(5))
-# print(cube(5))
-
-def apple(fx, value):
-    return 6 + fx(value)
-
-print(apple(lambda x : x * x, 5))
-
+l = [1, 2, 4, 6, 4, 3]
+x = list(map(lambda x : x**2, l))
+print(list(filter(lambda x : x > 19, x)))
