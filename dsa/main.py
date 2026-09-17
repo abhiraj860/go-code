e91a86df16fa62283bf473977f27ad37f1f5a5be
abhiraj <abhiaditya860@gmail.com>
2026-09-18 00:18:57 +0530

    magic methods

--- dsa/main.py
+++ dsa/main.py
@@ -1,16 +1,13 @@
 class Employee:
-    def __init__(self, name, id):
+    def __init__(self, name):
         self.name = name
-        self.id = id
 
-class Programmer(Employee):
-    def __init__(self, name, id, lang):
-        self.lang = lang
-        super().__init__(name, id)
-        self.lang
+    def __len__(self):
+        return len(self.name)
+
+    def __call__(self):
+        print("Hey I am good")
         
-rohan = Employee("Rohan Das", "420")
-harry = Programmer("Harry", "32345", "javascript")
-print(harry.name)
-print(harry.id)
-print(harry.lang)
+        
+e = Employee("Hqrry")
+e()
