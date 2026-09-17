d04234108a89c15d1319a49cd95f71b9c0170d9f
abhiraj <abhiaditya860@gmail.com>
2026-09-18 00:06:33 +0530

    super

--- dsa/main.py
+++ dsa/main.py
@@ -1,2 +1,16 @@
-x = [int(x.strip()) for x in input("Enter numbers: ").split(",")] 
-print(x)
+class Employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+class Programmer(Employee):
+    def __init__(self, name, id, lang):
+        self.lang = lang
+        super().__init__(name, id)
+        self.lang
+        
+rohan = Employee("Rohan Das", "420")
+harry = Programmer("Harry", "32345", "javascript")
+print(harry.name)
+print(harry.id)
+print(harry.lang)
