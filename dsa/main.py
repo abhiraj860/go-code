fa4cb6edcee8f24460592fda11e050c48721e24c
abhiraj <abhiaditya860@gmail.com>
2026-09-18 13:20:27 +0530

    async-gather-all

--- dsa/main.py
+++ dsa/main.py
@@ -1,14 +1,21 @@
 import asyncio
+import time
 
-async def fetchData():
-    print("Start fetching...")
-    await asyncio.sleep(2)
-    print("Data fetched!")
-    return {"data": 100} 
+async def downloadFile(fileId, duration):
+    print(f"File {fileId}: Starting Download...")
+    await asyncio.sleep(duration)
+    print(f"File {fileId}: Finished in {duration}s!")
+    return f"content_{fileId}"
 
 async def main():
-    result = await fetchData()
-    print(f"Result {result}")   
-
-print("Hello")
+    start = time.time()
+    result = await asyncio.gather(
+        downloadFile(1, 3),
+        downloadFile(2, 1),
+        downloadFile(3, 2),
+    )
+    
+    print(f"Download: {result}")
+    print(f"Total time: {time.time() - start:.2f} seconds")
+    
 asyncio.run(main())
