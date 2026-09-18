54ad885312910c5bf16f466b5c06ab07aea300be
abhiraj <abhiaditya860@gmail.com>
2026-09-18 13:25:04 +0530

    async-background-task

--- dsa/main.py
+++ dsa/main.py
@@ -1,21 +1,14 @@
 import asyncio
-import time
 
-async def downloadFile(fileId, duration):
-    print(f"File {fileId}: Starting Download...")
-    await asyncio.sleep(duration)
-    print(f"File {fileId}: Finished in {duration}s!")
-    return f"content_{fileId}"
-
+async def sendEmail(userId):
+    await asyncio.sleep(4)
+    print(f"Email sent to user {userId}")
+    
 async def main():
-    start = time.time()
-    result = await asyncio.gather(
-        downloadFile(1, 3),
-        downloadFile(2, 1),
-        downloadFile(3, 2),
-    )
-    
-    print(f"Download: {result}")
-    print(f"Total time: {time.time() - start:.2f} seconds")
-    
+    task = asyncio.create_task(sendEmail(100))
+    print("Processing HTTP request...")
+    await asyncio.sleep(1)
+    print("HTTP response returned to user!")
+    await task
+
 asyncio.run(main())
