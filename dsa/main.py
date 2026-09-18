600a67945ca5212e1988d004d64bca6ca404215a
abhiraj <abhiaditya860@gmail.com>
2026-09-18 13:28:45 +0530

    handle time outs

--- dsa/main.py
+++ dsa/main.py
@@ -1,14 +1,14 @@
 import asyncio
 
-async def sendEmail(userId):
-    await asyncio.sleep(4)
-    print(f"Email sent to user {userId}")
-    
-async def main():
-    task = asyncio.create_task(sendEmail(100))
-    print("Processing HTTP request...")
+async def slowApi():
     await asyncio.sleep(1)
-    print("HTTP response returned to user!")
-    await task
+    return "API Success"
 
+async def main():
+    try:
+        result = await asyncio.wait_for(slowApi(), timeout = 2.0)
+        print(result)
+    except asyncio.TimeoutError:
+        print("Reqiestion time out")
+        
 asyncio.run(main())
