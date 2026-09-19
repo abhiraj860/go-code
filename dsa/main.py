4877cf87f7dceeb725182a826694ea088dd4b2c2
abhiraj <abhiaditya860@gmail.com>
2026-09-19 10:33:44 +0530

    getattrr

--- dsa/main.py
+++ dsa/main.py
@@ -1,7 +1,2 @@
-class Employee:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-e = Employee("abhiraj", 45)
-print(getattr(e, "TTT", "454"))
+lst = [[]]
+print(len(lst))
