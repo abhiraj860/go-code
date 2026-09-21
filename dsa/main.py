4673a4f749dddb58832aae02b2e4d85e2d24b18d
abhiraj <abhiaditya860@gmail.com>
2026-09-22 00:21:37 +0530

    floyd-warshall-algo

--- dsa/main.py
+++ dsa/main.py
@@ -1,33 +1,38 @@
-from collections import deque
+# Number of vertices
+n = 4
 
-adjList = {    
-        0: [1, 3],    
-        1: [2],
-        2: [],
-        3: [1, 4, 5],
-        4: [5],
-        5: []
-} 
-
-
-def toposort(adjList):
-    order = []
-    n = len(adjList)
-    indegree = [0] * n
-    for v in adjList.values():
-        for a in v:
-            indegree[a] += 1
-            
-    queue = deque([v for v in indegree if v == 0])
-    while queue:
-        node = queue.popleft()
-        order.append(node)
-        for v in adjList[node]:
-            indegree[v] -= 1
-            if indegree[v] == 0:
-                queue.append(v)
-    
-    return order if n == len(order) else []
+# Edge list in the format (u, v, weight)
+graph = [
+    (0, 1, 5),
+    (0, 3, 10),
+    (1, 2, 3),
+    (2, 3, 1),
+    (3, 1, -10)  # Negative edge weight creates a negative cycle between 1, 2, and 3
+]
 
 
-print(toposort(adjList))
+def floyd_warshall(graph, n):
+    dist = [[float('inf')] * n for _ in range(n)]
+    for i in range(n):
+        dist[i][i]  = 0
+    for u, v, w in graph:
+        dist[u][v] = w
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    
+    for v in range(n):
+        if dist[v][v] < 0:
+            print("Negtive")
+            return None
+
+    return dist
+
+
+
+# Running your function with this test input
+result = floyd_warshall(graph, n)
+print(result)
