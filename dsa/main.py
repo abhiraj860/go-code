31a2b9b1a860c7d9b672f7b9b48a9f7423e43573
abhiraj <abhiaditya860@gmail.com>
2026-09-21 21:02:14 +0530

    toposort

--- dsa/main.py
+++ dsa/main.py
@@ -1,19 +1,33 @@
-edges = [(0, 1), (1, 2), (1, 3), (3, 2), (3, 4)]
-n = 5
+from collections import deque
 
-indegree = [0] * n
-for u, v in edges:
-    indegree[v] += 1
+adjList = {    
+        0: [1, 3],    
+        1: [2],
+        2: [],
+        3: [1, 4, 5],
+        4: [5],
+        5: []
+} 
+
+
+def toposort(adjList):
+    order = []
+    n = len(adjList)
+    indegree = [0] * n
+    for v in adjList.values():
+        for a in v:
+            indegree[a] += 1
+            
+    queue = deque([v for v in indegree if v == 0])
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+        for v in adjList[node]:
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                queue.append(v)
     
-print(indegree)
+    return order if n == len(order) else []
 
 
-edges = {0: [1], 1: [2, 3], 2: [], 3: [2, 4], 4: []}
-n = 5
-
-indegree = [0] * n
-for nbrs in edges.values():
-    for nbr in nbrs:
-        indegree[nbr] += 1
-        
-print(indegree)
+print(toposort(adjList))
