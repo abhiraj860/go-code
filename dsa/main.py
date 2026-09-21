bbea1cf1c6e1915bb97d1cb470f0e24fbc305d30
abhiraj <abhiaditya860@gmail.com>
2026-09-22 00:28:54 +0530

    bellman-ford

--- dsa/main.py
+++ dsa/main.py
@@ -1,38 +1,31 @@
-# Number of vertices
-n = 4
+# Graph configuration
+n = 5
+start = 0
 
-# Edge list in the format (u, v, weight)
-graph = [
-    (0, 1, 5),
-    (0, 3, 10),
+# Edge list: (u, v, weight)
+edges = [
+    (0, 1, -1),
+    (0, 2, 4),
     (1, 2, 3),
-    (2, 3, 1),
-    (3, 1, -10)  # Negative edge weight creates a negative cycle between 1, 2, and 3
+    (1, 3, 2),
+    (1, 4, 2),
+    (3, 2, 5),
+    (3, 1, 1),
+    (4, 3, -3)
 ]
 
-
-def floyd_warshall(graph, n):
-    dist = [[float('inf')] * n for _ in range(n)]
-    for i in range(n):
-        dist[i][i]  = 0
-    for u, v, w in graph:
-        dist[u][v] = w
-
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
-                    
-    for v in range(n):
-        if dist[v][v] < 0:
-            print("Negtive")
+def bellman_ford(edges, n, start):
+    dist = [float("inf")] * n
+    dist[start]= 0
+    for v in range(n - 1):
+        for u, v, w in edges:
+            if dist[u] + w < dist[v]:
+                dist[v] = dist[u] + w
+    for u, v, w in edges:
+        if dist[u] + w < dist[v]:
             return None
-
     return dist
 
-
-
-# Running your function with this test input
-result = floyd_warshall(graph, n)
-print(result)
+# Running your function
+result = bellman_ford(edges, n, start)
+print("Distances from start node:", result)
