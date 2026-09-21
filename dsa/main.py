5ff8bc88768de4e835a51c2be0e5697106b61aa4
abhiraj <abhiaditya860@gmail.com>
2026-09-21 20:51:40 +0530

    indegrees

--- dsa/main.py
+++ dsa/main.py
@@ -1,3 +1,19 @@
-s = [["aaa", "bbb"], ["ccc", "ddd"], ["eee", "fff"]]
-s1 = [1, 2, 3, 4]
-print([k for k in s1])
+edges = [(0, 1), (1, 2), (1, 3), (3, 2), (3, 4)]
+n = 5
+
+indegree = [0] * n
+for u, v in edges:
+    indegree[v] += 1
+    
+print(indegree)
+
+
+edges = {0: [1], 1: [2, 3], 2: [], 3: [2, 4], 4: []}
+n = 5
+
+indegree = [0] * n
+for nbrs in edges.values():
+    for nbr in nbrs:
+        indegree[nbr] += 1
+        
+print(indegree)
